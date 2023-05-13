9c344317f432853af95ba3b235717ce6cfea3999
imJack6 <3486688394@qq.com>
2023-05-13 20:24:30 +0800

    Improve some of the main classes

--- src/main/java/cn/jackcraft/runforyourlife/RunForYourLife.java
+++ src/main/java/cn/jackcraft/runforyourlife/RunForYourLife.java
@@ -3,6 +3,8 @@ package cn.jackcraft.runforyourlife;
 import cn.jackcraft.runforyourlife.handle.ConfigHandler;
 import cn.jackcraft.runforyourlife.handle.LangHandler;
 import cn.jackcraft.runforyourlife.helper.LoggerHelper;
+import cn.jackcraft.runforyourlife.literature.StringPool;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,43 +18,63 @@ public final class RunForYourLife extends JavaPlugin implements Listener {
     public static LoggerHelper globalLogger;
     public static ConfigHandler globalConfig;
     public static LangHandler globalLang;
+    public static final Locale DEFAULT_SYSTEM_LOCALE = Locale.getDefault();
+    public static StringPool globalStringPool = new StringPool();
     @Override
     public void onLoad(){
         // 插件加载逻辑 - 1
         saveDefaultConfig();
         plugin = this;
-        // 初始化日志
-        globalLogger = new LoggerHelper();
         // 初始化配置文件
         globalConfig = new ConfigHandler(this);
+        // 初始化日志记录器
+        globalLogger = new LoggerHelper();
         // 初始化语言
+        globalLogger.debug("Initialize local language...");
         globalLang = new LangHandler(this);
+
+        globalLogger.debug("The Load phase ends");
     }
     @Override
     public void onEnable() {
         // 插件加载逻辑 - 2
-
-        /* Simplified Chinese */
-        final Locale locale = Locale.getDefault();
-        if (locale.getLanguage().equals("zh") && locale.getCountry().equals("CN")) {
-            globalLogger.Info("欢迎使用 逃往生处 插件！");
-        } else /* American English */ {
-            globalLogger.Info("You're using plugin \"Run for your life\", Hooray!");
+        globalLogger.debug("Start the Enable phase...");
+        globalLogger.debug("Registering for events...");
+        // 注册事件
+        Bukkit.getPluginManager().registerEvents(this,this);
+        globalLogger.debug("Registering for commands...");
+        // 注册指令
+        getCommand("RunForYourLife").setExecutor(new PluginCommands());
+        globalLogger.debug("Detecting the presence of the PlaceholderAPI...");
+        if (!hasPlaceholderAPI()) {
+            globalLogger.info(globalStringPool.getFromPool(StringPool.TextSelections.placeholderAPINotFound));
         }
-
+        globalLogger.info(globalStringPool.getFromPool(StringPool.TextSelections.welcome));
+        globalLogger.debug("Detects if the battlefield or profile has been initialized...");
+        if (!globalConfig.getGameInit()) {
+            globalLogger.info(globalLang.getLang("SYSTEM.NOT_INIT"));
+        }
+        globalLogger.debug("The Enable phase ends");
     }
     @Override
     public void onDisable() {
         // 插件禁用逻辑
+        globalLogger.debug("Start the Disable phase...");
+        globalLogger.info(globalStringPool.getFromPool(StringPool.TextSelections.bye));
+        globalLogger.debug("The Disable phase ends");
+    }
+    public static boolean hasPlaceholderAPI(){
+        return (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null);
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
-/*        if (!ConfigIsInit()){
-            if (player.isOp()) {
-
+        final Player p = event.getPlayer();
+        globalLogger.debug("Player " + p.getName() + " joins. op=" + p.isOp() + " Player=" + p);
+        if (!globalConfig.getGameInit()){
+            if (p.isOp()) {
+                globalLogger.playerMsg(p, globalLang.getLang("SYSTEM.NOT_INIT"));
             }
-        }*/
+        }
     }
 }
