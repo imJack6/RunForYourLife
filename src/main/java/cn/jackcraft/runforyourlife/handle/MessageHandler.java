510ec7fcacdd69266c8fea9223213c33938fb0ab
imJack6 <3486688394@qq.com>
2023-05-13 20:23:33 +0800

    Add Placeholder API processing

--- src/main/java/cn/jackcraft/runforyourlife/handle/MessageHandler.java
+++ src/main/java/cn/jackcraft/runforyourlife/handle/MessageHandler.java
@@ -1,7 +1,13 @@
 package cn.jackcraft.runforyourlife.handle;
 
+import me.clip.placeholderapi.PlaceholderAPI;
+import me.clip.placeholderapi.PlaceholderHook;
 import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import static cn.jackcraft.runforyourlife.RunForYourLife.hasPlaceholderAPI;
 
 public class MessageHandler {
     public static String getConsoleMessageHandling(String inputString) {
@@ -14,7 +20,15 @@ public class MessageHandler {
     public static String getPlayerMessageHandling(Player player, String inputString) {
         String handle = inputString;
         handle = ChatColor.translateAlternateColorCodes('&', handle);
+        handle = getPAPIHandling(player, handle);
         // ...
+        return handle;
+    }
+
+    public static String getPAPIHandling(Player player, String inputString){
+        String handle = inputString;
+        if (hasPlaceholderAPI())
+            handle = PlaceholderAPI.setPlaceholders(player, handle);
         return handle;
     }
 }
