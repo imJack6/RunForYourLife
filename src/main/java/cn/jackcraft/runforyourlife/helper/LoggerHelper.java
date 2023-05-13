2e9899a7228e9cf06049d17fc50c355826d60f0c
imJack6 <3486688394@qq.com>
2023-05-13 20:22:32 +0800

    Improve the Logger class

--- src/main/java/cn/jackcraft/runforyourlife/helper/LoggerHelper.java
+++ src/main/java/cn/jackcraft/runforyourlife/helper/LoggerHelper.java
@@ -4,7 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import cn.jackcraft.runforyourlife.handle.ConfigHandler;
+import java.util.logging.Logger;
 
 import static cn.jackcraft.runforyourlife.RunForYourLife.globalConfig;
 import static cn.jackcraft.runforyourlife.handle.MessageHandler.getConsoleMessageHandling;
@@ -12,10 +12,12 @@ import static cn.jackcraft.runforyourlife.handle.MessageHandler.getPlayerMessage
 
 public class LoggerHelper {
     private final CommandSender GLOBAL_COMMAND_SENDER;
+    private final Logger GLOBAL_BUKKIT_LOGGER;
 
     public LoggerHelper()
     {
         this.GLOBAL_COMMAND_SENDER = Bukkit.getConsoleSender();
+        this.GLOBAL_BUKKIT_LOGGER = Bukkit.getLogger();
     }
     public void Info(String Msg) {
         Info(true, Msg);
@@ -25,13 +27,40 @@ public class LoggerHelper {
                 ? getConsoleMessageHandling(globalConfig.getPluginPrefix() + " " + msg)
                 : getConsoleMessageHandling(msg));
     }
+    public void warn(String msg) {
+        warn(true, msg);
+    }
+    public void warn(boolean hasPrefix, String msg) {
+        GLOBAL_BUKKIT_LOGGER.warning(hasPrefix ? ("[RunForYourLife] " + msg) : msg);
+    }
 
-    public void PlayerMsg(Player player, String msg) {
-        PlayerMsg(true, player, msg);
+    public void debug(String msg){
+        if (globalConfig.getPluginDebugMode())
+            GLOBAL_COMMAND_SENDER.sendMessage(getConsoleMessageHandling("&7[&6RunForYourLife&7] &c[Debug] &7" + msg));
+    }
+
+    public void playerMsg(Player player, String msg) {
+        playerMsg(true, player, msg);
     }
     public void PlayerMsg(boolean hasPrefix, Player player, String msg) {
         player.sendMessage((hasPrefix)
                 ? getPlayerMessageHandling(player, globalConfig.getPluginPrefix() + " " + msg)
                 : getPlayerMessageHandling(player, msg));
+    }
+
+    public void commandSenderMsg(CommandSender sender, String msg) {
+        commandSenderMsg(true, sender, msg);
+    }
+    public void commandSenderMsg(boolean hasPrefix, CommandSender sender, String msg) {
+        if (sender instanceof Player){
+            Player p = ((Player) sender).getPlayer();
+            sender.sendMessage((hasPrefix)
+                    ? getPlayerMessageHandling(p, globalConfig.getPluginPrefix() + " " + msg)
+                    : getPlayerMessageHandling(p, msg));
+        } else {
+            sender.sendMessage((hasPrefix)
+                    ? getConsoleMessageHandling(globalConfig.getPluginPrefix() + " " + msg)
+                    : getConsoleMessageHandling(msg));
+        }
     }
 }
